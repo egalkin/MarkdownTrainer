a4b8363dc2f9254190a7de9fd911702c84762716
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-08 23:45:49 +0300

    Added api for settings and failed tries

--- trainer.app/trainer/api_folder/admin_api.py
+++ trainer.app/trainer/api_folder/admin_api.py
@@ -1,7 +1,8 @@
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
-from trainer.api_folder.api_utils import upload_images, get_random_list
+from trainer import failed_deque, failed_parser, trainer_settings, settings_parser
+from trainer.api_folder.api_utils import upload_images, get_random_list, add_to_deque
 from trainer.marshmallow_schemas import TaskSchema
 
 
@@ -9,9 +10,33 @@ class TaskCreation(Resource):
     task_schema_list = TaskSchema(many=True)
 
     def get(self):
-        list_lim = request.args['lim']
+        list_lim = 0
         return self.task_schema_list.dump(get_random_list(int(list_lim))).data
 
     def post(self):
         return upload_images(request.files), 200
 
+
+class FailedDeque(Resource):
+    task_schema = TaskSchema()
+
+    def get(self):
+        if len(failed_deque) > 0:
+            return self.task_schema.dump(failed_deque.popleft()).data
+        else:
+            return None
+
+    def post(self):
+        args = failed_parser.parse_args()
+        return add_to_deque(args), 200
+
+
+class TrainerSettings(Resource):
+    def get(self):
+        return trainer_settings, 200
+
+    def post(self):
+        args = settings_parser.parse_args()
+        for k, v in args.items():
+            trainer_settings[k] = v
+        return trainer_settings
