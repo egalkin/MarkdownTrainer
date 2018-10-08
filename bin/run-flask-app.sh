5e046f3f598f16a21a7cb2e2fd1321c92923e0bb
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-08 16:17:42 +0300

    Added app running script

---
+++ bin/run-flask-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+virtualenv -p python3 ./trainer.app/.venv
+source ./trainer.app/.venv/bin/activate
+
+pip3 install -r ./trainer.app/requirements.txt
+#!/bin/bash
+
+cd trainer.app
+
+source .venv/bin/activate
+source ../.env.local
+pip3 install -r requirements.txt
+
+export FLASK_APP=runserver.py
+flask run --port=5000
+
