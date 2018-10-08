f6b9229fb844f4cf16a9e3a72360c7ed61120d50
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-08 23:44:51 +0300

    Added routes for settings and failed deque

--- trainer.app/trainer/api_routes.py
+++ trainer.app/trainer/api_routes.py
@@ -1,4 +1,6 @@
 from trainer import api
-from trainer.api_folder.admin_api import TaskCreation
+from trainer.api_folder.admin_api import TaskCreation, FailedDeque, TrainerSettings
 
 api.add_resource(TaskCreation, '/tasks')
+api.add_resource(FailedDeque, '/failed')
+api.add_resource(TrainerSettings, '/settings')
