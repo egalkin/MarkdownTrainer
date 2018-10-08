71746f9c4255ed90be81fcc126c307eb5c96fcfe
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-08 16:04:39 +0300

    Added api routes

--- trainer.app/trainer/api_routes.py
+++ trainer.app/trainer/api_routes.py
@@ -1,4 +1,4 @@
 from trainer import api
-from trainer.api_folder.admin_api import Admin
+from trainer.api_folder.admin_api import TaskCreation
 
-api.add_resource(Admin, '/admin')
+api.add_resource(TaskCreation, '/tasks')
