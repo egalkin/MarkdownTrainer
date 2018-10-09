5a88ba2c33e3b708bc038a0e5134f5d9ffa050a1
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-09 11:27:29 +0300

    Added Parent dicrectory constant

--- trainer.app/trainer/config.py
+++ trainer.app/trainer/config.py
@@ -1,4 +1,4 @@
-
 class Config():
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/trainer.db'
-    TASK_IMAGE_FOLDER = 'static/tasks/images'
+    TASK_IMAGE_PARENT_FOLDER = 'trainer'
+    TASK_RELATIVE_PATH = 'static/tasks'
