3b347dc0cf3496723b8b9bf8e6c3ee36b05f3c51
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-10 15:39:42 +0300

    Add type anotations for admin_api.py

--- trainer.app/trainer/api_folder/admin_api.py
+++ trainer.app/trainer/api_folder/admin_api.py
@@ -9,33 +9,33 @@ from trainer.marshmallow_schemas import TaskSchema
 class TaskCreation(Resource):
     task_schema_list = TaskSchema(many=True)
 
-    def get(self):
+    def get(self) -> list:
         list_lim = 0 if 'len_of_list' not in trainer_settings else int(trainer_settings['len_of_list'])
         return self.task_schema_list.dump(get_random_list(int(list_lim))).data
 
-    def post(self):
+    def post(self) -> tuple:
         return upload_images(request.files), 200
 
 
 class FailedDeque(Resource):
     task_schema = TaskSchema()
 
-    def get(self):
+    def get(self) -> dict or None:
         if len(failed_deque) > 0:
             return self.task_schema.dump(failed_deque.popleft()).data
         else:
             return None
 
-    def post(self):
+    def post(self) -> tuple:
         print(request.data)
         return add_to_deque(json.loads(request.data)), 200
 
 
 class TrainerSettings(Resource):
-    def get(self):
+    def get(self) -> tuple:
         return trainer_settings, 200
 
-    def post(self):
+    def post(self) -> dict:
         args = settings_parser.parse_args()
         for k, v in args.items():
             trainer_settings[k] = v
